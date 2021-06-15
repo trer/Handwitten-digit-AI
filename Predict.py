5720d43f20143787e3f24837054f9b8563602d73
tordstr <tordstr@stud.ntnu.no>
2021-06-15 16:10:39 +0200

    creating a bare bones NN with keras

--- Predict.py
+++ Predict.py
@@ -1,12 +1,25 @@
 from FetchData import FetchData
-import torch
-from torch import tensor
-
-class Net:
-    def __init__(self, input):
-        self.input = input
-        self.layer1 = tensor([28,28], dtype= torch.float64)
-        print(self.layer1)
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+import numpy as np
 
 dataFetcher = FetchData()
-new = Net(dataFetcher.get_traningset())
+X = np.array(dataFetcher.get_traningset())
+y = np.array(dataFetcher.get_traninglabels())
+
+model = Sequential()
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10))
+
+
+model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, epochs=10, batch_size=10)
+
+testX = np.array(dataFetcher.get_testset())
+testy = np.array(dataFetcher.get_testlabels())
+
+_, accuracy = model.evaluate(testX, testy)
+print('Accuracy: %.2f' % (accuracy*100))
